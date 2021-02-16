210c8391974c7754dccbd703f25b83ffa6d7841e
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-16 23:13:16 +0100

    Doubly Linked List
    The more complicated of the two lists, a doubly linked lists' nodes, in addition to contain their own data and a pointer to the next node, also contain a pointer to the previous node in the list.

--- Lists/Doubly_Linked_List.java
+++ Lists/Doubly_Linked_List.java
@@ -9,33 +9,33 @@ public class Doubly_Linked_List {
         }  
     }  										
   
-    Node head, tail = null;  				// Same as singly linked list, head and tail are null
+    Node head, tail = null;  				// Same as singly linked list, head and tail are null.
   
-    public void addNode(int data) {  		// Function adds a node to the list
-        Node newNode = new Node(data);  	// First, the new node is created
+    public void addNode(int data) {  		// Function adds a node to the list.
+        Node newNode = new Node(data);  	// First, the new node is created.
   
-        if(head == null) {					// If list is empty  
-            head = tail = newNode;			// Both head and tail will point to newNode    
-            head.previous = null;           // Head's previous will point to null  
-            tail.next = null;  				// Tail's next will point to null - casue new node is the last node in the list
+        if(head == null) {					// If list is empty.  
+            head = tail = newNode;			// Both head and tail will point to newNode.    
+            head.previous = null;           // Head's previous will point to null.  
+            tail.next = null;  				// Tail's next will point to null - casue new node is the last node in the list.
         }  
         else {  
-            tail.next = newNode;  			// newNode will be added after tail such that tail's next will point to newNode   
-            newNode.previous = tail;  		// newNode's previous will point to tail, which is in fact the new node  
-            tail = newNode;  				// newNode becomes the new tail  
-            tail.next = null;  				// As it is last node, tail's next will point to null
+            tail.next = newNode;  			// newNode will be added after tail such that tail's next will point to newNode.  
+            newNode.previous = tail;  		// newNode's previous will point to tail, which is in fact the new node.  
+            tail = newNode;  				// newNode becomes the new tail.  
+            tail.next = null;  				// As it is last node, tail's next will point to null.
         }  
     }  
   
-    public void display() {  				// Method to output the doubly linked list 
-        Node current = head; 				// Node current points to head  
+    public void display() {  				// Method to output the doubly linked list. 
+        Node current = head; 				// Node current points to head.  
         if(head == null) {  
             System.out.println("List is empty");  
             return;  
         }  
         System.out.println("Nodes of doubly linked list: ");  
         
-        while(current != null) {   			// If it is NOT null, it is a node within the list
+        while(current != null) {   			// If it is NOT null, it is a node within the list.
             System.out.print(current.data + " ");  
             current = current.next;		    // Prints each node by incrementing the pointer.   
         }  
@@ -48,8 +48,8 @@ public class Doubly_Linked_List {
         dList.addNode(2);  
         dList.addNode(3);  
         dList.addNode(4);  
-        dList.addNode(5);  					  // Adds nodes to the list  
+        dList.addNode(5);  					  // Adds nodes to the list.  
 
-        dList.display();  					  // Displays the list 
+        dList.display();  					  // Displays the list. 
     }  
 }  
